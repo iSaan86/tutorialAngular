fac8ac267c04c31597175f5608bc0cb4a71bd333
iSaan86 <sandraferreraarenas@gmail.com>
2025-03-09 14:50:17 +0100

    Primer commit: Añadiendo el proyecto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
-  title = 'app-tutorial';
+  title: string = 'homes';
 }
